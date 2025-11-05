fd4628484e753c880fd65e7aaa05db9939d44dac
jpachebat <63541985+jpachebat@users.noreply.github.com>
2025-11-05 16:00:43 +0100

    fix: update OSC52 plugin file with conditional loading

--- nvim/lua/neotex/plugins/tools/osc52.lua
+++ nvim/lua/neotex/plugins/tools/osc52.lua
@@ -1,30 +1,29 @@
 -- OSC 52 clipboard plugin for SSH/remote environments
 -- Provides reliable copy/paste over SSH connections
+--
+-- Usage on SSH/cluster:
+--   Visual mode: select text, then <leader>c to copy to local clipboard
+--   Normal mode: <leader>cc to copy current line
+--
+-- Usage locally: Normal y/p commands work with system clipboard
 return {
   "ojroques/nvim-osc52",
   event = "VeryLazy",
+  cond = function()
+    -- Only load on SSH connections
+    return vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil
+  end,
   config = function()
     require('osc52').setup({
       max_length = 0,           -- Maximum length of selection (0 for no limit)
-      silent = false,           -- Disable message on successful copy
-      trim = false,             -- Trim surrounding whitespaces before copy
-      tmux_passthrough = true,  -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+      silent = false,           -- Show message on successful copy
+      trim = false,             -- Don't trim whitespaces
+      tmux_passthrough = true,  -- Use tmux passthrough if in tmux
     })
 
-    -- Automatically copy to clipboard when yanking in visual/visual-line mode
-    local function copy()
-      if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-        require('osc52').copy_register('+')
-      end
-    end
-
-    vim.api.nvim_create_autocmd('TextYankPost', {
-      callback = copy
-    })
-
-    -- Keybindings for manual copy
-    vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true, desc = "OSC52 copy"})
-    vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true, desc = "OSC52 copy line"})
-    vim.keymap.set('v', '<leader>c', require('osc52').copy_visual, {desc = "OSC52 copy visual"})
+    -- Manual copy keybindings (only when SSH'd)
+    vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true, desc = "Copy to local (OSC52)"})
+    vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true, desc = "Copy line to local"})
+    vim.keymap.set('v', '<leader>c', require('osc52').copy_visual, {desc = "Copy selection to local"})
   end,
 }
