3de89bebdf98c12eb42f29a458f705a94324c936
Jean Pachebat <jean.pachebat@polytechnique.edu>
2025-11-18 19:06:32 +0100

    added wqueue function

--- shell/local.sh
+++ shell/local.sh
@@ -1,4 +1,4 @@
 # Machine-specific / secret config.
 # Example:
-# export WANDB_API_KEY="YOUR_REAL_KEY"
+export WANDB_API_KEY="0e768875870d095d197e5fcb0dd999f83311fa11"
 # export OPENAI_API_KEY="..."
